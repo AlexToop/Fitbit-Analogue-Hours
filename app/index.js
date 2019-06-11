239e48724f3de5f7af90c7770e9861c37899e16e
Alex Toop <alextoop@outlook.com>
2019-06-11 18:48:33 +0100

    Dots view added to settings. Better colour selection support added.

--- app/index.js
+++ app/index.js
@@ -32,6 +32,22 @@ const no9 = document.getElementById('no9');
 const no10 = document.getElementById('no10');
 const no11 = document.getElementById('no11');
 const no12 = document.getElementById('no12');
+const no1dot = document.getElementById('no1dot');
+const no2dot = document.getElementById('no2dot');
+const no3dot = document.getElementById('no3dot');
+const no4dot = document.getElementById('no4dot');
+const no5dot = document.getElementById('no5dot');
+const no6dot = document.getElementById('no6dot');
+const no7dot = document.getElementById('no7dot');
+const no8dot = document.getElementById('no8dot');
+const no9dot = document.getElementById('no9dot');
+const no10dot = document.getElementById('no10dot');
+const no11dot = document.getElementById('no11dot');
+const no12dot = document.getElementById('no12dot');minutes_bar
+
+const hoursColourBar = document.getElementById('hours-hand');
+const minutesColourBar = document.getElementById('minutes-hand');
+const timeMiddleDotColour = document.getElementById('timeMiddleDot');
 // for battery saving
 const sensors = [];
 var noBatteryDots = 5;
@@ -104,11 +120,84 @@ messaging.peerSocket.onmessage = evt => {
     no10.style.fill = color;
     no11.style.fill = color;
     no12.style.fill = color;
+    no1dot.style.fill = color;
+    no2dot.style.fill = color;
+    no3dot.style.fill = color;
+    no4dot.style.fill = color;
+    no5dot.style.fill = color;
+    no6dot.style.fill = color;
+    no7dot.style.fill = color;
+    no8dot.style.fill = color;
+    no9dot.style.fill = color;
+    no10dot.style.fill = color;
+    no11dot.style.fill = color;
+    no12dot.style.fill = color;
 
     statsText1.style.fill = color;
     statsDivi.style.fill = color;
     statsText2.style.fill = color;
     dateText.style.fill = color;
+
+
+    hoursColourBar.style.fill = color;
+    minutesColourBar.style.fill = color;
+    timeMiddleDotColour.style.fill = color;
+  }
+  if (evt.data.key === "numbersStyle" && evt.data.newValue) {
+    let styleString = JSON.parse(evt.data.newValue);
+    styleString = styleString.values[0]["name"];
+    if (styleString == "All numbers"){
+      no1.style.display = "inline";
+      no2.style.display = "inline";
+      no3.style.display = "inline";
+      no4.style.display = "inline";
+      no5.style.display = "inline";
+      no6.style.display = "inline";
+      no7.style.display = "inline";
+      no8.style.display = "inline";
+      no9.style.display = "inline";
+      no10.style.display = "inline";
+      no11.style.display = "inline";
+      no12.style.display = "inline";
+      no1dot.style.display = "none";
+      no2dot.style.display = "none";
+      no3dot.style.display = "none";
+      no4dot.style.display = "none";
+      no5dot.style.display = "none";
+      no6dot.style.display = "none";
+      no7dot.style.display = "none";
+      no8dot.style.display = "none";
+      no9dot.style.display = "none";
+      no10dot.style.display = "none";
+      no11dot.style.display = "none";
+      no12dot.style.display = "none";
+    }
+    if (styleString == "Numbers and dots"){
+      no1.style.display = "none";
+      no2.style.display = "none";
+      no3.style.display = "none";
+      no4.style.display = "none";
+      no5.style.display = "none";
+      no6.style.display = "none";
+      no7.style.display = "none";
+      no8.style.display = "none";
+      no9.style.display = "none";
+      no10.style.display = "none";
+      no11.style.display = "none";
+      no12.style.display = "none";
+      no1dot.style.display = "inline";
+      no2dot.style.display = "inline";
+      no3dot.style.display = "inline";
+      no4dot.style.display = "inline";
+      no5dot.style.display = "inline";
+      no6dot.style.display = "inline";
+      no7dot.style.display = "inline";
+      no8dot.style.display = "inline";
+      no9dot.style.display = "inline";
+      no10dot.style.display = "inline";
+      no11dot.style.display = "inline";
+      no12dot.style.display = "inline";
+    }
   }
 };
 
--- settings/index.jsx
+++ settings/index.jsx
@@ -1,36 +1,38 @@
 function mySettings(props) {
+    let colorSet = [
+        {color: "black"}, {color: "#334764"}, {color: "#2A363B"}, {color: "#A6AEAC"}, {color: "#c4b9a4"}, {color: "#E2B292"}, {color: "#f57d26"}, {color: "#E84A5F"},  {color: "#DE2626"}
+    ];
     return (
         <Page>
             <Section
-                title={<Text bold align="center">Colour of left-to-minute hand</Text>}>
+                title={<Text bold align="center">Colour of left-to-minute hand (Usually dark)</Text>}>
                 <ColorSelect
                     settingsKey="leftColour"
-                    colors={[
-                        {color: "#334764"},
-                        {color: "#2A363B"}
-                    ]}
+                    colors={colorSet}
                 />
             </Section>
             <Section
-                title={<Text bold align="center">Colour of right-to-minute hand</Text>}>
+                title={<Text bold align="center">Colour of right-to-minute hand (Usually light)</Text>}>
                 <ColorSelect
                     settingsKey="rightColour"
-                    colors={[
-                        {color: "#c4b9a4"},
-                        {color: "#FECEA8"},
-                        {color: "#dddddd"},
-                        {color: "#f57d26"},
-                        {color: "#2A363B"}
-                    ]}
+                    colors={colorSet}
                 />
             </Section>
             <Section
                 title={<Text bold align="center">Colour of time and activities</Text>}>
                 <ColorSelect
                     settingsKey="timeActivitiesColour"
-                    colors={[
-                        {color: "#f57d26"},
-                        {color: "#E84A5F"}
+                    colors={colorSet}
+                />
+            </Section>
+            <Section
+                title={<Text bold align="center">Number style</Text>}>
+                <Select
+                    label={`Select style:`}
+                    settingsKey="numbersStyle"
+                    options={[
+                        {name: "All numbers"},
+                        {name: "Numbers and dots"}
                     ]}
                 />
             </Section>
@@ -39,15 +41,3 @@ function mySettings(props) {
 }
 
 registerSettingsPage(mySettings);
-
-// <Section
-//   title={<Text bold align="center">Weather Provider (Default OpenWeatherMap)</Text>}>
-//   <Select
-//     label={`Provider Selection:`}
-//     settingsKey="weather"
-//     options={[
-//       {name:"OpenWeatherMap"},
-//       {name:"Dark Sky"}
-//     ]}
-//   />
-// </Section>
